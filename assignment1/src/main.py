1d63cba0c5d79cf87336d5fe86f98fcaeb7f2ee9
WonHyung Shin <wonhyung64@icloud.com>
2022-03-14 21:50:52 +0900

    hybrid image

--- assignment1/src/main.py
+++ assignment1/src/main.py
@@ -8,28 +8,36 @@ from utils import (
     preprocessing,
     draw_array
 )
-tf.__version__
 
-print(21)
 #%%
 if __name__ == "__main__":
     imgs = load_data()
     img0, img1, img2, img3 = preprocessing(imgs)
-    
     draw_array(img0)
     draw_array(img1)
-    draw_array(img2)
-    draw_array(img3)
 
+    filter0 = tfa.image.gaussian_filter2d(img0, filter_shape=(7,7), sigma=30.0)
+    filter0 = tf.cast(filter0, dtype=tf.float32)
+    draw_array(filter0)
+
+    filter1 = (tfa.image.gaussian_filter2d(img1, filter_shape=(7,7), sigma=20.0) - img1)
+    filter1 = tf.cast(filter1, dtype=tf.float32)
+    draw_array(filter1)
+
+    draw_array(filter0 + filter1)
+
+
+
+
+    filter2 = tfa.image.gaussian_filter2d(img2, filter_shape=(7,7), sigma=30.0)
+    filter2 = tf.cast(filter2, dtype=tf.float32)
+    draw_array(filter2)
+
+    filter3 = (tfa.image.gaussian_filter2d(img3, filter_shape=(7,7), sigma=30.0) - img3)
+    filter3 = tf.cast(filter3, dtype=tf.float32)
+    draw_array(filter3)
+
+    draw_array(filter2 + filter3)
 
 
 #%%
-
-
-tf.keras.preprocessing.image.array_to_img(
-    tfa.image.mean_filter2d(img2, constant_values=4) - img2
-)
-tf.keras.preprocessing.image.array_to_img(
-    tfa.image.gaussian_filter2d(img2, sigma=4.0) - img2
-)
-tf.keras.preprocessing.image.array_to_img(img2 - img2)
--- assignment1/src/utils/data_utils.py
+++ assignment1/src/utils/data_utils.py
@@ -14,8 +14,7 @@ def load_data() -> List:
         List: imgs
     """
     cwd = Path(os.getcwd())
-    parent_wd = cwd.parent
-    ex_dir = f"{parent_wd}/ex"
+    ex_dir = f"{cwd}/ex"
     img_lst = os.listdir(ex_dir)
 
     imgs = []
@@ -23,6 +22,7 @@ def load_data() -> List:
         img_dir = f"{ex_dir}/{img_lst[i]}"
         img = Image.open(img_dir)
         img = tf.convert_to_tensor(img)
+        img = tf.cast(img, dtype=tf.float32)
         imgs = [img] + imgs
 
     return imgs
@@ -39,10 +39,8 @@ def preprocessing(imgs: List) -> List:
     Returns:
         List: preprocessed imgs
     """
-    img0 = imgs[0][100:230, 175:305, ...]
-    img0 = tf.image.resize(img0, (200, 200))
-    img1 = imgs[1][35:155, 165:285, ...]
-    img1 = tf.image.resize(img1, (200, 200))
+    img0 = tf.image.resize(imgs[0], (200, 200))
+    img1 = tf.image.resize(imgs[1], (200, 200))
     img2 = imgs[2][35:235, 85:285, ...]
     img3 = imgs[3][35:235, 135:335, ...]
     imgs = [img0, img1, img2, img3]
