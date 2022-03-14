25767ce0f537534997f9f018563cfd550c2aebb5
WonHyung Shin <wonhyung64@icloud.com>
2022-03-14 19:30:36 +0900

    add draw utils

--- assignment1/src/utils/__init__.py
+++ assignment1/src/utils/__init__.py
@@ -2,3 +2,6 @@ from .data_utils import (
     load_data,
     preprocessing,
 )
+from .draw_utils import (
+    draw_array,
+)
---
+++ assignment1/src/utils/draw_utils.py
@@ -0,0 +1,9 @@
+#%%
+import tensorflow as tf
+
+#%%
+def draw_array(img):
+    return tf.keras.preprocessing.image.array_to_img(img)
+
+
+# %%
