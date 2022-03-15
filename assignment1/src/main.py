7c31e6719f3bf64ce833d33458509e127d89ae0a
WonHyung Shin <wonhyung64@icloud.com>
2022-03-15 10:56:32 +0900

    clean code

--- assignment1/src/main.py
+++ assignment1/src/main.py
@@ -3,11 +3,8 @@ import tensorflow as tf
 import tensorflow_addons as tfa
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
-from utils import (
-    load_data,
-    preprocessing,
-    draw_array
-)
+import matplotlib
+from utils import load_data, preprocessing, draw_array
 
 #%%
 if __name__ == "__main__":
@@ -16,24 +13,21 @@ if __name__ == "__main__":
     draw_array(img0)
     draw_array(img1)
 
-    filter0 = tfa.image.gaussian_filter2d(img0, filter_shape=(7,7), sigma=30.0)
+    filter0 = tfa.image.gaussian_filter2d(img0, filter_shape=(7, 7), sigma=30.0)
     filter0 = tf.cast(filter0, dtype=tf.float32)
     draw_array(filter0)
 
-    filter1 = (tfa.image.gaussian_filter2d(img1, filter_shape=(7,7), sigma=20.0) - img1)
+    filter1 = tfa.image.gaussian_filter2d(img1, filter_shape=(7, 7), sigma=20.0) - img1
     filter1 = tf.cast(filter1, dtype=tf.float32)
     draw_array(filter1)
 
     draw_array(filter0 + filter1)
 
-
-
-
-    filter2 = tfa.image.gaussian_filter2d(img2, filter_shape=(7,7), sigma=30.0)
+    filter2 = tfa.image.gaussian_filter2d(img2, filter_shape=(7, 7), sigma=30.0)
     filter2 = tf.cast(filter2, dtype=tf.float32)
     draw_array(filter2)
 
-    filter3 = (tfa.image.gaussian_filter2d(img3, filter_shape=(7,7), sigma=30.0) - img3)
+    filter3 = tfa.image.gaussian_filter2d(img3, filter_shape=(7, 7), sigma=30.0) - img3
     filter3 = tf.cast(filter3, dtype=tf.float32)
     draw_array(filter3)
 
