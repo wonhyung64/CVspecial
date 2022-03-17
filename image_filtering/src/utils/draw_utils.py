b81716bb57f2094a41c3ae8aa71b38be402b7f31
WonHyung Shin <wonhyung64@icloud.com>
2022-03-18 02:10:13 +0900

    add draw outputs fcn

--- image_filtering/src/utils/draw_utils.py
+++ image_filtering/src/utils/draw_utils.py
@@ -16,5 +16,17 @@ def array_to_img(img: tf.Tensor) -> None:
     return img
 
 
-def draw_outputs(imgs: PIL.Image.Image) -> None:
-    
+def draw_outputs(imgs: PIL.Image.Image, cmap: str or None) -> None:
+    """
+    draw imgs
+
+    Args:
+        imgs (PIL.Image.Image): result imgs
+        cmap (strorNone): color map option
+    """
+    plt.rcParams["figure.figsize"] = (20.0, 80.0)
+    for i in range(len(imgs)):
+        plt.subplot(1, 4, i + 1)
+        plt.axis("off")
+        plt.imshow(imgs[i], cmap=cmap)
+    plt.show()
