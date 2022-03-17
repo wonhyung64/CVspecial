6cc7372d65bb7ff53f168710c542247838aadab0
WonHyung Shin <wonhyung64@icloud.com>
2022-03-18 00:13:50 +0900

    adapt fcns

--- assignment1/src/main.py
+++ assignment1/src/main.py
@@ -3,30 +3,47 @@ import tensorflow as tf
 import tensorflow_addons as tfa
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
-from utils import load_data, preprocessing, draw_array
-
+from utils import (
+    load_data, 
+    preprocessing, 
+    draw_array,
+    gaussian_filter,
+    mean_filter,
+    sharpen_filter,
+    gray_filter,
+    thresholding_filter,
+    highpass_filter,
+)
 #%%
 if __name__ == "__main__":
     imgs = load_data()
     img0, img1, img2, img3 = preprocessing(imgs)
 
-    filter0 = tfa.image.gaussian_filter2d(img0, filter_shape=(7, 7), sigma=30.0)
+    filter0 = gaussian_filter(img0, 7, 30.)
     draw_array(filter0)
 
-    filter1 = tfa.image.gaussian_filter2d(img1, filter_shape=(7, 7), sigma=20.0) - img1
+    filter1 = gaussian_filter(img1, 7, 30.)
     draw_array(filter1)
 
-    draw_array(filter0 + filter1)
+    filter1_detail = highpass_filter(img1, filter1)
+    draw_array(filter1_detail)
+    
+    draw_array(filter0 + filter1_detail)
 
-    filter2 = tfa.image.gaussian_filter2d(img2, filter_shape=(7, 7), sigma=30.0)
+    filter2 = gaussian_filter(img2, 7, 30.)
     draw_array(filter2)
 
-    filter3 = tfa.image.gaussian_filter2d(img3, filter_shape=(7, 7), sigma=30.0) - img3
+    filter3 = gaussian_filter(img3, 7, 30.)
     draw_array(filter3)
 
-    draw_array(filter2 + filter3)
+    filter3_detail = highpass_filter(img3, filter3)
+    draw_array(filter3_detail)
+
+    draw_array(filter2 + filter3_detail)
 
     draw_array(tfa.image.mean_filter2d(img0, filter_shape=(7,7)))
+    
+
 
 
 #%%
