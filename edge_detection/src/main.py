ece8369dc4bb0da064908944222821cb6108168d
WonHyung Shin <wonhyung64@icloud.com>
2022-04-01 20:26:38 +0900

    show outputs

--- edge_detection/src/main.py
+++ edge_detection/src/main.py
@@ -11,41 +11,39 @@ from utils import (
 )
 
 #%%
+if
 imgs = load_data()
 img = imgs[0]
 
-
-
 img_gray = gray_filter(img)
-array_to_img(img_gray)
-
-
 img_blur = gaussian_filter(img_gray, 7, 0.)
-array_to_img(img_blur)
-
 img_blur_log = laplassian_filter(img_gray, 5, 1)
-array_to_img(img_blur_log)
-#%%
-    
+
 sobel_x = sobel_edge(img_blur, "x", 5)
 sobel_y = sobel_edge(img_blur, "y", 5)
 sobel_xy = sobel_edge(img_blur, "xy", 5)
+
+array_to_img(img)
+array_to_img(img_gray)
+array_to_img(img_blur)
+array_to_img(img_blur_log)
 
 array_to_img(sobel_x)
 array_to_img(sobel_y)
 array_to_img(sobel_xy)
 array_to_img(sobel_x + sobel_y)
 
+array_to_img(thresholding_filter(sobel_xy, 20))
+array_to_img(thresholding_filter(sobel_xy, 30))
+array_to_img(thresholding_filter(sobel_xy, 40))
+array_to_img(thresholding_filter(sobel_xy, 50))
 
-threshold = 127.5
-sobel = thresholding_filter(sobel_x + sobel_y, threshold)
-array_to_img(sobel)
+array_to_img(canny_edge(img_blur, threshold1=10, threshold2=70))
+array_to_img(canny_edge(img_blur, threshold1=10, threshold2=90))
+array_to_img(canny_edge(img_blur, threshold1=10, threshold2=110))
+array_to_img(canny_edge(img_blur, threshold1=10, threshold2=130))
 
-
-gaussian_canny = canny_edge(img_blur, threshold1=100, threshold2=200.)
-log_canny = canny_edge(img_blur_log, threshold1=100, threshold2=200)
-array_to_img(gaussian_canny)
-array_to_img(log_canny)
-
-
-# %%
+array_to_img(thresholding_filter(img_blur_log, 100))
+array_to_img(thresholding_filter(img_blur_log, 150))
+array_to_img(thresholding_filter(img_blur_log, 200))
+array_to_img(thresholding_filter(img_blur_log, 250))
