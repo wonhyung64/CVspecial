409ab0fbd14a1d93de253bdd7da0d9a4e786a396
WonHyung Shin <wonhyung64@icloud.com>
2022-04-01 20:33:48 +0900

    show outputs

--- edge_detection/src/main.py
+++ edge_detection/src/main.py
@@ -2,6 +2,7 @@
 from utils import (
     load_data,
     array_to_img,
+    draw_outputs,
     gray_filter,
     thresholding_filter,
     gaussian_filter,
@@ -11,39 +12,64 @@ from utils import (
 )
 
 #%%
-if
-imgs = load_data()
-img = imgs[0]
+if __name__ == "__main__":
+    imgs = load_data()
+    img = imgs[0]
 
-img_gray = gray_filter(img)
-img_blur = gaussian_filter(img_gray, 7, 0.)
-img_blur_log = laplassian_filter(img_gray, 5, 1)
+    img_gray = gray_filter(img)
+    img_blur = gaussian_filter(img_gray, 7, 0.)
+    img_blur_log = laplassian_filter(img_gray, 5, 1)
 
-sobel_x = sobel_edge(img_blur, "x", 5)
-sobel_y = sobel_edge(img_blur, "y", 5)
-sobel_xy = sobel_edge(img_blur, "xy", 5)
+    sobel_x = sobel_edge(img_blur, "x", 5)
+    sobel_y = sobel_edge(img_blur, "y", 5)
+    sobel_xy = sobel_edge(img_blur, "xy", 5)
 
-array_to_img(img)
-array_to_img(img_gray)
-array_to_img(img_blur)
-array_to_img(img_blur_log)
+    original = [
+        array_to_img(img),
+        array_to_img(img_gray),
+        array_to_img(img_blur),
+        array_to_img(img_blur_log),
+    ]
 
-array_to_img(sobel_x)
-array_to_img(sobel_y)
-array_to_img(sobel_xy)
-array_to_img(sobel_x + sobel_y)
+    sobel = [
+        array_to_img(sobel_x),
+        array_to_img(sobel_y),
+        array_to_img(sobel_xy),
+        array_to_img(sobel_x + sobel_y),
+    ]
 
-array_to_img(thresholding_filter(sobel_xy, 20))
-array_to_img(thresholding_filter(sobel_xy, 30))
-array_to_img(thresholding_filter(sobel_xy, 40))
-array_to_img(thresholding_filter(sobel_xy, 50))
+    sobel_thresholding = [
+        array_to_img(thresholding_filter(sobel_xy, 20)), 
+        array_to_img(thresholding_filter(sobel_xy, 30)),
+        array_to_img(thresholding_filter(sobel_xy, 40)),
+        array_to_img(thresholding_filter(sobel_xy, 50)),
+    ]
 
-array_to_img(canny_edge(img_blur, threshold1=10, threshold2=70))
-array_to_img(canny_edge(img_blur, threshold1=10, threshold2=90))
-array_to_img(canny_edge(img_blur, threshold1=10, threshold2=110))
-array_to_img(canny_edge(img_blur, threshold1=10, threshold2=130))
+    canny_gaussian = [
+        array_to_img(canny_edge(img_blur, threshold1=10, threshold2=70)), 
+        array_to_img(canny_edge(img_blur, threshold1=10, threshold2=90)), 
+        array_to_img(canny_edge(img_blur, threshold1=10, threshold2=110)),
+        array_to_img(canny_edge(img_blur, threshold1=10, threshold2=130)),
+    ]
 
-array_to_img(thresholding_filter(img_blur_log, 100))
-array_to_img(thresholding_filter(img_blur_log, 150))
-array_to_img(thresholding_filter(img_blur_log, 200))
-array_to_img(thresholding_filter(img_blur_log, 250))
+    canny_laplassian = [
+        array_to_img(thresholding_filter(img_blur_log, 100)),
+        array_to_img(thresholding_filter(img_blur_log, 150)),
+        array_to_img(thresholding_filter(img_blur_log, 200)),
+        array_to_img(thresholding_filter(img_blur_log, 250)),
+    ]
+
+    result = [
+        original,
+        sobel,
+        sobel_thresholding,
+        canny_gaussian,
+        canny_laplassian,
+    ]
+
+    for idx in range(len(result)):
+        if idx == 3 or idx == 4:
+            cmap = "gray"
+        else:
+            cmap = None
+        draw_outputs(result[idx], cmap)
--- edge_detection/src/utils/draw_utils.py
+++ edge_detection/src/utils/draw_utils.py
@@ -1,6 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import PIL
 from PIL import Image
+
 
 
 def array_to_img(img: np.ndarray) -> PIL.Image:
@@ -16,3 +18,19 @@ def array_to_img(img: np.ndarray) -> PIL.Image:
     img = Image.fromarray(img)
 
     return img
+
+
+def draw_outputs(imgs: PIL.Image.Image, cmap: str or None) -> None:
+    """
+    draw imgs
+
+    Args:
+        imgs (PIL.Image.Image): result imgs
+        cmap (strorNone): color map option
+    """
+    plt.rcParams["figure.figsize"] = (20.0, 80.0)
+    for i in range(len(imgs)):
+        plt.subplot(1, 4, i + 1)
+        plt.axis("off")
+        plt.imshow(imgs[i], cmap=cmap)
+    plt.show()
